5f86ccd86469405c5f6d3f4123846a4c3d98fb91
dinarte <dinarte@gmail.com>
2024-11-07 17:12:57 -0500

    removing migrations project pakage

--- bmodule-picklist/src/main/java/com/quebecteh/modules/migrators/repository/JDBCSourceModelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quebecteh.modules.migrators.repository;
-
-import com.quebecteh.commons.multitenancy.MultiTenancyCrudRepository;
-import com.quebecteh.modules.migrators.domain.JDBCSourceModel;
-
-public interface JDBCSourceModelRepository extends MultiTenancyCrudRepository<JDBCSourceModel, Long> {
-
-}
--- bmodule-picklist/src/main/java/com/quebecteh/modules/migrators/repository/JSonSourceModelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quebecteh.modules.migrators.repository;
-
-import com.quebecteh.commons.multitenancy.MultiTenancyCrudRepository;
-import com.quebecteh.modules.migrators.domain.JSonSourceModel;
-
-public interface JSonSourceModelRepository extends MultiTenancyCrudRepository<JSonSourceModel, Long> {
-
-}
